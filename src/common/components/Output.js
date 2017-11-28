adc567a367d5a2056a35a701fe158d5ac8ef47e0
dgeibi <png.inside@gmail.com>
2017-11-28 16:01:19 +0800

    输出可以折行

--- src/common/components/Output.js
+++ src/common/components/Output.js
@@ -2,13 +2,24 @@ import React from 'react'
 import './Output.css'
 
 function Output({
-  children, value, className, ...props
+  wrap = false, style, children, value, className, ...props
 }) {
   return (
     <div
       {...props}
       className={className}
       styleName="output"
+      style={Object.assign(
+        wrap
+          ? {
+              wordWrap: 'break-word',
+              whiteSpace: 'pre-wrap',
+            }
+          : {
+              whiteSpace: 'pre',
+            },
+        style
+      )}
       children={children || value || '无数据'}
     />
   )
