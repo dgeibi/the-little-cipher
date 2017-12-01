403ffd4a386a808d2ec828278096a77f6ea8eaa0
Jiabin Peng <png.inside@gmail.com>
2017-12-02 01:37:45 +0800

    Fix: 空白菜单项

--- src/common/routes/NotFound.js
+++ src/common/routes/NotFound.js
@@ -2,5 +2,6 @@ import React from 'react'
 import { Route, Redirect } from 'dva/router'
 
 const NotFound = () => <Route render={() => <Redirect to="/" />} />
+NotFound.skipMenu = true
 
 export default NotFound
