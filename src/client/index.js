b159e9cbe7118e555d5f9251f08cab0c5dd58fad
dgeibi <png.inside@gmail.com>
2017-12-03 21:21:21 +0800

    生成代码时读取目录

--- src/client/index.js
+++ src/client/index.js
@@ -16,7 +16,11 @@ const app = dva({
 
 // use codegen to generate `app.model(require('...'))` and hmr code
 codegen(`
-  const modelPaths = require('../common/models/paths.json').map(x => '../common/models/' + x)
+  const fs = require('fs')
+  const regExp = /.+\\.js$/
+  const modelPaths = fs.readdirSync(__dirname + '/../common/models/')
+    .filter(x => regExp.test(x))
+    .map(x => '../common/models/' + x)
   module.exports = modelPaths.map(m => 'app.model(require("'+m+'").default)').join(';')
   if (process.env.NODE_ENV === 'development') {
     module.exports += require('../webpack/modelHMR')(modelPaths)
