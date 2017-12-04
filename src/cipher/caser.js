8f0e8781a3ad13e9e649371e3b45155e546f48de
dgeibi <png.inside@gmail.com>
2017-12-04 18:20:30 +0800

    Cipher: 添加凯撒密码的解密算法

--- src/cipher/caser.js
+++ src/cipher/caser.js
@@ -2,15 +2,30 @@ import { codeOf, isUpperCase } from '../util'
 
 const offset = codeOf('A')
 
-const caser = str =>
-  String(str)
-    .split('')
-    .map((c) => {
-      const C = c.toUpperCase()
-      if (!isUpperCase(C)) return c
-      const code = (codeOf(C) - offset + 3) % 26 + offset
-      return String.fromCharCode(code)
-    })
-    .join('')
+/**
+ * 返回处理字符串的函数
+ * @param {function} handleChar 处理字符的函数
+ */
+const map = handleChar => str => String(str).split('').map(handleChar).join('')
 
-export default caser
+/**
+ * 凯撒密码加密算法
+ */
+const caser = map((c) => {
+  const C = c.toUpperCase()
+  if (!isUpperCase(C)) return c
+  const code = (codeOf(C) - offset + 3) % 26 + offset
+  return String.fromCharCode(code)
+})
+
+/**
+ * 凯撒密码解密算法
+ */
+const decode = map((c) => {
+  const C = c.toUpperCase()
+  if (!isUpperCase(C)) return c
+  const code = (codeOf(C) - offset + 23) % 26 + offset
+  return String.fromCharCode(code)
+})
+
+export { caser as default, caser as encode, decode }
--- src/cipher/caser.test.js
+++ src/cipher/caser.test.js
@@ -1,7 +1,13 @@
-import caser from './caser'
+import caser, { decode } from './caser'
 
 test('works', () => {
   expect(caser('abC')).toBe('DEF')
   expect(caser('xyz')).toBe('ABC')
   expect(caser('你真Good')).toBe('你真JRRG')
+})
+
+test('decode works', () => {
+  expect(decode('abc')).toBe('XYZ')
+  expect(decode('def')).toBe('ABC')
+  expect(decode('你真JRRG')).toBe('你真GOOD')
 })
