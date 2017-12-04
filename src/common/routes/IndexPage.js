1be1db7442b35fba70016d7b34f06999299c0c2c
dgeibi <png.inside@gmail.com>
2017-12-04 17:43:54 +0800

    更新首页

--- src/common/routes/IndexPage.js
+++ src/common/routes/IndexPage.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 function IndexPage() {
-  return <div>hello world</div>
+  return (
+    <Fragment>
+      <p>如菜单所示，本应用实现了三种古典密码，其中凯撒密码、Hill密码在浏览器计算， Playfair密码在服务器计算，都支持文本和文件输入。</p>
+      <p>
+        源码已发布在 <a href="https://github.com/dgeibi/the-little-cipher">GitHub</a>。
+      </p>
+    </Fragment>
+  )
 }
 
 IndexPage.title = '首页'
