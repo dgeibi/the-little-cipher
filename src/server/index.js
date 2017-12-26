204ab442b90524369b08c60b61b2678ce076f0cf
dgeibi <png.inside@gmail.com>
2017-12-26 18:37:19 +0800

    (server): set max-age to 365d for js/css

--- src/server/index.js
+++ src/server/index.js
@@ -2,4 +2,15 @@ import express from 'express'
 import { join } from 'path'
 import createServer from './createServer'
 
-createServer(express.static(join(__dirname, '../static')))
+createServer(
+  express.static(join(__dirname, '../static'), {
+    maxAge: '365d',
+    setHeaders: setCustomCacheControl,
+  })
+)
+
+function setCustomCacheControl(res, path) {
+  if (express.static.mime.lookup(path) === 'text/html') {
+    res.setHeader('Cache-Control', 'public, max-age=0')
+  }
+}
