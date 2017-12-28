6c54b402017cb8f575d5f0cd1c36acd4949de6af
dgeibi <png.inside@gmail.com>
2017-12-28 13:51:24 +0800

    use tmp dir

--- src/server/createServer.js
+++ src/server/createServer.js
@@ -1,18 +1,20 @@
 import express from 'express'
 import compression from 'compression'
 import multer from 'multer'
+import os from 'os'
+import path from 'path'
 import playfair from './playfair'
 import notFound from './notFound'
 
 export default function createServer(staticMiddleware) {
-  const upload = multer({ dest: 'uploads/' })
+  const upload = multer({ dest: path.join(os.tmpdir(), 'uploads') })
   const app = express()
   app.use(compression())
 
   app.use(staticMiddleware)
 
   // playfair api
-  app.post('/playfair', upload.single('plaintext'), playfair)
+  app.post('/playfair', upload.array(), playfair)
 
   // handle Not Found
   app.use(notFound)
