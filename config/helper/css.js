c31a6ed38b6051d978656dd2ad6021b136f8c473
dgeibi <png.inside@gmail.com>
2018-01-16 20:42:34 +0800

    minor change

--- config/helper/css.js
+++ config/helper/css.js
@@ -1,12 +1,15 @@
+const buildRule = require('./rule')
+
 module.exports = ({ ssr, rule, extractOptions, extract }) => {
-  if (!rule || !rule.use) {
-    throw Error('rule and rule.use required')
+  if (!rule || !(rule.use || rule.loader)) {
+    throw Error('rule.use || rule.loader required')
   }
+
   const mRule = Object.assign({}, rule)
   let use
-  if (Array.isArray(rule.use) && rule.use[0]) {
+  if (Array.isArray(mRule.use) && rule.use[0]) {
     use = mRule.use.slice()
-  } else if (rule.loader || rule.use) {
+  } else if (rule.loader || (typeof rule.use === 'string' && rule.use)) {
     const { loader, options } = rule
     delete mRule.loader
     delete mRule.use
@@ -19,25 +22,28 @@ module.exports = ({ ssr, rule, extractOptions, extract }) => {
   } else {
     throw Error('can not find rule use')
   }
-  if (ssr === true) {
-    if (use[0].loader && !/css-loader/.test(use[0].loader))
-      throw Error('You may write wrong!')
-    use[0].loader = 'css-loader/locals'
-    return {
-      module: { rules: [Object.assign(mRule, { use })] },
-    }
+
+  if (use[0].loader && !/css-loader/.test(use[0].loader)) {
+    use.unshift({ loader: 'css-loader' })
   }
+
+  mRule.use = use
+
+  if (ssr === true) {
+    mRule.use[0].loader = 'css-loader/locals'
+    return buildRule(mRule)
+  }
+
   if (extract && extractOptions) {
     const ExtractTextPlugin = require('extract-text-webpack-plugin')
     const extractor = new ExtractTextPlugin(extractOptions)
-    use = extractor.extract(use)
+    mRule.use = extractor.extract(mRule.use)
     return {
       plugins: [extractor],
-      module: { rules: [Object.assign(mRule, { use })] },
+      module: { rules: [mRule] },
     }
   }
-  use.unshift('style-loader')
-  return {
-    module: { rules: [Object.assign(mRule, { use })] },
-  }
+
+  mRule.use.unshift('style-loader')
+  return buildRule(mRule)
 }
